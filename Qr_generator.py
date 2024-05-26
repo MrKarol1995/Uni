21969b532c8e6e5cd74f5ff7a4f442918e497d2d
Karol Cieślik <275976@student.pwr.edu.pl>
2024-05-26 17:50:21 +0200

    Update Qr_generator.py

--- Qr_generator.py
+++ Qr_generator.py
@@ -1,16 +1,20 @@
 import qrcode
 import cv2
 
-def generate_qr_code(text, file_name):
+
+def generate_qr_code(text: str, file_name: str) -> None:
     """
     Generuje kod QR z podanym tekstem i zapisuje go do pliku.
 
+    Args:
+        text (str): Tekst do zakodowania w QR.
+        file_name (str): Nazwa pliku, do którego zostanie zapisany kod QR.
     """
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
-        border=4
+        border=4,
     )
     qr.add_data(text)
     qr.make(fit=True)
@@ -19,10 +23,16 @@ def generate_qr_code(text, file_name):
     img.save(file_name)
     print(f'Kod QR został zapisany do pliku: {file_name}')
 
-def read_qr_code(file_name):
+
+def read_qr_code(file_name: str) -> str:
     """
     Odczytuje zawartość kodu QR z obrazka.
 
+    Args:
+        file_name (str): Nazwa pliku z kodem QR.
+
+    Returns:
+        str: Odczytany tekst z kodu QR lub None, jeśli nie udało się odczytać.
     """
     image = cv2.imread(file_name)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -45,6 +55,5 @@ qqr = "kod2.jpg"
 # Generowanie kodu QR
 generate_qr_code(text, qr_file_name)
 
+# Odczyt kodu QR
 read_qr_code(qqr)
-
-
