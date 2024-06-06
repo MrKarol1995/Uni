b60cb1bf1c8f34aaf2840f4ff6d187342bb7862f
Karol Cieślik <275976@student.pwr.edu.pl>
2024-06-06 22:27:45 +0200

    Update Mixed_Poisson_process.py

--- Mixed_Poisson_process.py
+++ Mixed_Poisson_process.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple, List
 
+
 def mixed_poisson_process_uniform(T: float) -> Tuple[int, np.ndarray]:
     """
     Generuje trajektorię procesu Poissona z wartością lambda z rozkładu jednostajnego.
@@ -13,7 +14,9 @@ def mixed_poisson_process_uniform(T: float) -> Tuple[int, np.ndarray]:
     - I (int): Liczba zdarzeń.
     - S (array): Tablica zawierająca czasy zdarzeń.
     """
-    lmbda = np.random.uniform(0, 10)  # Generowanie losowej wartości lambda z rozkładu jednostajnego
+    lmbda = np.random.uniform(
+        0, 10
+    )  # Generowanie losowej wartości lambda z rozkładu jednostajnego
     t = 0
     I = 0
     S = []
@@ -25,6 +28,7 @@ def mixed_poisson_process_uniform(T: float) -> Tuple[int, np.ndarray]:
         I += 1
         S.append(t)
     return I, np.array(S)
+
 
 def mixed_poisson_process_exponential(T: float) -> Tuple[int, np.ndarray]:
     """
@@ -37,7 +41,9 @@ def mixed_poisson_process_exponential(T: float) -> Tuple[int, np.ndarray]:
     - I (int): Liczba zdarzeń.
     - S (array): Tablica zawierająca czasy zdarzeń.
     """
-    lmbda = np.random.exponential(1)  # Generowanie losowej wartości lambda z rozkładu wykładniczego
+    lmbda = np.random.exponential(
+        1
+    )  # Generowanie losowej wartości lambda z rozkładu wykładniczego
     t = 0
     I = 0
     S = []
@@ -50,6 +56,7 @@ def mixed_poisson_process_exponential(T: float) -> Tuple[int, np.ndarray]:
         S.append(t)
     return I, np.array(S)
 
+
 # Parametr czasu T
 T_val = 29
 
@@ -58,25 +65,25 @@ plt.figure(figsize=(15, 6))
 
 # Generowanie trajektorii dla rozkładu jednostajnego
 plt.subplot(1, 2, 1)
-plt.title('10 trajektorii dla lambda z rozkładu jednostajnego')
+plt.title("10 trajektorii dla lambda z rozkładu jednostajnego")
 for _ in range(10):
     I, S = mixed_poisson_process_uniform(T_val)
-    plt.step(S, np.arange(len(S)), where='post', label=f'Trajektoria')
-plt.xlabel('Czas')
-plt.ylabel('Liczba zdarzeń')
+    plt.step(S, np.arange(len(S)), where="post", label=f"Trajektoria")
+plt.xlabel("Czas")
+plt.ylabel("Liczba zdarzeń")
 plt.legend()
-plt.grid(linestyle='--')
+plt.grid(linestyle="--")
 
 # Generowanie trajektorii dla rozkładu wykładniczego
 plt.subplot(1, 2, 2)
-plt.title('10 trajektorii dla lambda z rozkładu wykładniczego')
+plt.title("10 trajektorii dla lambda z rozkładu wykładniczego")
 for _ in range(10):
     I, S = mixed_poisson_process_exponential(T_val)
-    plt.step(S, np.arange(len(S)), where='post', label=f'Trajektoria')
-plt.xlabel('Czas')
-plt.ylabel('Liczba zdarzeń')
+    plt.step(S, np.arange(len(S)), where="post", label=f"Trajektoria")
+plt.xlabel("Czas")
+plt.ylabel("Liczba zdarzeń")
 plt.legend()
-plt.grid(linestyle='--')
+plt.grid(linestyle="--")
 
 plt.tight_layout()
 plt.show()
